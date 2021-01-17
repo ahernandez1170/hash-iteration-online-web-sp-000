afede1b0e7307425f6d5f7740d89573ddcbb1d90
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-17 15:22:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,10 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids = {
+  birthday_kids = {Timmy: 9,
+                    Sarah:6,
+                    Amanda:27
+                  }
 end
 
 
